a03531d13ef32437f3a6c656a2616546fa317a68
Jan Königsmark <j.konigsmark@gmail.com>
2023-09-02 14:04:05 +0200

    Add new breakpoint.scss file

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,7 @@
 import style from "../styles/Header.module.scss";
 
 export const Header = () => {
-  return (
-    <div className={style.header}>
-      <h1>RoadToStar</h1>
-    </div>
-  );
+  <div className={style.header}>
+    <h1>RoadToStar</h1>
+  </div>;
 };
