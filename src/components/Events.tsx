f0c58f4844c71ec72f51a037e1686786d3bf0afd
pexomat <33259845+pexomat@users.noreply.github.com>
2024-10-29 14:15:52 +0100

    Event detail (#3)

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -3,10 +3,12 @@ import { useEffect, useState } from "react";
 import type { Event } from "../model/Event";
 import { Loader } from "../components/Loader";
 import style from "./Events.module.scss";
+import { useRouter } from "next/router";
 
 export const Events: React.FC = () => {
   const [events, setEvents] = useState<Event[]>([]);
   const [isLoading, setIsLoading] = useState(true);
+  const router = useRouter();
 
   useEffect(() => {
     const fetchEvents = async () => {
@@ -20,6 +22,12 @@ export const Events: React.FC = () => {
     fetchEvents();
   }, []);
 
+  const handleRedirect = (event: Event) => {
+    router.push({
+      pathname: `/event/${event.id}`,
+    });
+  };
+
   if (isLoading) {
     return <Loader />;
   }
@@ -27,7 +35,11 @@ export const Events: React.FC = () => {
   return (
     <ul className={style.list}>
       {events.map((event: Event) => (
-        <li key={event.id} className={style.event}>
+        <li
+          key={event.id}
+          className={style.event}
+          onClick={() => handleRedirect(event)}
+        >
           <div className={style.sport}>{event.sport}</div>
           <div>From: {event.name}</div>
         </li>
---
+++ src/pages/event/[id].tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Loader } from "../../components/Loader";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import type { Event } from "../../model/Event";
+
+const EventDetail: React.FC = () => {
+  const router = useRouter();
+  const { id } = router.query;
+  const [event, setEvent] = useState<Event>();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchEvent = async () => {
+      const response = await fetch(
+        `https://sportujspolu-api.onrender.com/api/v1/events/${id}`
+      );
+      setEvent(await response.json());
+      setIsLoading(false);
+    };
+
+    fetchEvent();
+  }, [id]);
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  return (
+    <div>
+      <p>Name: {event?.name}</p>
+      <p>Sport: {event?.sport}</p>
+      <p>Id: {id}</p>
+      <button onClick={() => router.push("/")}>Back</button>
+    </div>
+  );
+};
+
+export default EventDetail;
